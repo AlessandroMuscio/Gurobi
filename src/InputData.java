ebd1a90d8b8fee6db854738947e7927d4c0aead6
AlessandroMuscio <muscioalex30@gmail.com>
2022-06-06 15:23:51 +0200

    Creazione classe InputData

---
+++ src/InputData.java
@@ -0,0 +1,30 @@
+public class InputData {
+  public int v;
+  public int a;
+  public int[] b1b2;
+  public int c;
+  public int[] d1d2;
+  public int[] e1e2;
+  public int[] f1f2;
+  public int[] g1g2;
+  public int[] h1h2;
+  public int[] i1i2;
+  public int l;
+  public int[][] graph;
+
+  public InputData(int v, int a, int[] b1b2, int c, int[] d1d2, int[] e1e2, int[] f1f2, int[] g1g2,
+      int[] h1h2, int[] i1i2, int l, int[][] graph) {
+    this.v = v;
+    this.a = a;
+    this.b1b2 = b1b2;
+    this.c = c;
+    this.d1d2 = d1d2;
+    this.e1e2 = e1e2;
+    this.f1f2 = f1f2;
+    this.g1g2 = g1g2;
+    this.h1h2 = h1h2;
+    this.i1i2 = i1i2;
+    this.l = l;
+    this.graph = graph;
+  }
+}
